99a3cbf68d383eaaab39b67cd3a944e5ec73242a
Wei Kai <827056678@qq.com>
2021-07-16 16:30:17 +0800

    update

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -22,12 +22,29 @@ class UserFactory extends Factory
      */
     public function definition()
     {
+        $avatars = [
+            'https://cdn.learnku.com/uploads/images/201710/14/1/s5ehp11z6s.png',
+            'https://cdn.learnku.com/uploads/images/201710/14/1/Lhd1SHqu86.png',
+            'https://cdn.learnku.com/uploads/images/201710/14/1/LOnMrqbHJn.png',
+            'https://cdn.learnku.com/uploads/images/201710/14/1/xAuDMxteQy.png',
+            'https://cdn.learnku.com/uploads/images/201710/14/1/ZqM7iaP4CR.png',
+            'https://cdn.learnku.com/uploads/images/201710/14/1/NDnzMutoxX.png',
+        ];
+        // 随机取一个月以内的时间
+        $updated_at = $this->faker->dateTimeThisYear();
+        // 为创建时间传参，意为最大不超过 $updated_at，因为创建时间需永远比更改时间要早
+        $created_at = $this->faker->dateTimeThisYear($updated_at);
+
         return [
-            'name' => $this->faker->name(),
-            'email' => $this->faker->unique()->safeEmail(),
-            'email_verified_at' => now(),
+            'name' => $this->faker->name,
+            'email' => $this->faker->unique()->safeEmail,
+            'email_verified_at' => $created_at,
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'remember_token' => Str::random(10),
+            'introduction' => $this->faker->sentence(),
+            'avatar' => $this->faker->randomElement($avatars),
+            'created_at' => $created_at,
+            'updated_at' => $updated_at,
         ];
     }
 
